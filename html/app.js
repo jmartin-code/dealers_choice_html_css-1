9f784eff1dafc23ce161360e5021a19ef4b823f8
Lorenzo Noel <lnoel1299@gmail.com>
2022-05-04 18:08:01 -0400

    new commit

---
+++ html/app.js
@@ -0,0 +1,10 @@
+
+
+function toggle(){
+const div= document.getElementById("subMenu")
+if(div.style.display === 'none'){
+    div.style.display = 'block';
+}else{
+    div.style.display = 'none';
+}
+}
